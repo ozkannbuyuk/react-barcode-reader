572b2304eef4dccdf4e1741050af9458c9a50333
Özkan Büyük <ozkanbuyuk@icloud.com>
2024-02-28 01:07:17 +0300

    React - Barcode Reader

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useEffect, useState } from "react";
 
 function App() {
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [barcode, setBarcode] = useState(null);
+  const [basket, setBasket] = useState([]);
+
+  const openCamera = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: {
+          width: 300,
+          height: 300,
+          facingMode: "environment",
+        },
+      })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+
+        const ctx = canvasRef.current.getContext("2d");
+        const barcodeDetector = new window.BarcodeDetector({
+          formats: ["qr_code", "ean_13"],
+        });
+        setInterval(() => {
+          canvasRef.current.width = videoRef.current.videoWidth;
+          canvasRef.current.height = videoRef.current.videoHeight;
+          ctx.drawImage(
+            videoRef.current,
+            0,
+            0,
+            videoRef.current.videoWidth,
+            videoRef.current.videoHeight
+          );
+          barcodeDetector
+            .detect(canvasRef.current)
+            .then(([detectedBarcode]) => {
+              if (detectedBarcode) {
+                setBarcode(detectedBarcode.rawValue);
+              }
+            })
+            .catch((err) => console.error(err));
+        }, 100);
+      })
+      .catch((err) => alert(err));
+  };
+
+  useEffect(() => {
+    if (barcode) {
+      const API_URL = `http://localhost/api.php?barcode=${barcode}`;
+
+      fetch(API_URL)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data) {
+            setBasket([...basket, data]);
+          } else {
+            alert("This product was not found!");
+          }
+        })
+        .catch((error) => console.error("Error fetching data:", error));
+    }
+  }, [barcode, basket]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={openCamera}>Open Camera</button>
+      <div>
+        <video ref={videoRef} autoPlay muted hidden />
+        <canvas ref={canvasRef} />
+      </div>
+      {barcode && <div>Found barcode: {barcode}</div>}
+      {basket &&
+        basket.map((item) => (
+          <div key={item.id}>
+            {item.product} <br />
+            {item.price} <br />
+            <img
+              src={item.image}
+              style={{ width: 100, height: 100 }}
+              alt={item.product}
+            />
+          </div>
+        ))}
+    </>
   );
 }
 
